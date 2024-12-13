a5d2105817d3c7dbecaf48b939a64a3891eadf06
nunkesser <robin.nunkesser@hshl.de>
2024-12-13 17:11:24 +0100

    End of 20241213 lecture

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  tasks = [
+    { number: 1, title: "Meeting with Susan"},
+    { number: 2, title: "Trip to Berlin"},
+    { number: 3, title: "Board meeting"},
+  ];
+
+  constructor(public alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+  async addTask() {
+    const alert = await this.alertCtrl.create({
+      header: 'Add a task',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          placeholder: 'Task title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            this.tasks.push({ number: this.tasks.length + 1, title: data.title });
+          }
+        }]
+      });
+        await alert.present();
+  }
 }
